ddf7804a4be14657b782a96bfb45751ab135bff0
Tyson Lackey <lackeyt90@gmail.com>
2020-07-17 15:12:56 -0700

    update UI for new prices and options

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,9 +7,9 @@ namespace PierresBakery.Models
     static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery");
-      Console.WriteLine("What type of bread would you like to add to your order? (white, sourdough, baguette, italian)");
+      Console.WriteLine("What type of bread would you like to add to your order? (white ($5), sourdough ($8), baguette ($3), italian ($7))");
       string breadType = Console.ReadLine();
-      Console.WriteLine("How many loaves of bread ($5 each) would you like? (Buy 2 get your 3rd free)");
+      Console.WriteLine("How many loaves of bread would you like? (Buy 2 get your 3rd free)");
       Bread newBread = new Bread(breadType, Console.ReadLine());
       while (!newBread.isBreadTypeValid())
       {
@@ -18,14 +18,14 @@ namespace PierresBakery.Models
       }
       while (!newBread.IsBreadOrderNumberValid())
       {
-        Console.WriteLine("-invalid input- Please enter a whole number of bread loafs ($5 each) to order (Buy 2 get your 3rd free):");
+        Console.WriteLine("-invalid input- Please enter a whole number of bread loafs ($5 each) to order (Buy 2 get your 3rd free, or buy 21 and two of them will be free):");
         newBread.BreadOrderNumberString = Console.ReadLine();
       }
       Console.WriteLine("How many pastries ($2 each, three for $5) would you like? ");
       Pastry newPastry = new Pastry(Console.ReadLine());
       while (!newPastry.IsPastryOrderValid())
       {
-        Console.WriteLine("-invalid input- Please enter a whole number of pastries ($2 each, three for $5) to order:");
+        Console.WriteLine("-invalid input- Please enter a whole number of pastries ($2 each, three for $5, 12 for $18) to order:");
         newPastry.PastryOrderString = Console.ReadLine();
       }
       newBread.BreadCostCalc();
