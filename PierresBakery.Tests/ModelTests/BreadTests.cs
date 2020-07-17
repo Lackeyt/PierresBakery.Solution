c8c36471e8253eb18b48a3533a81e067d9535dcd
Tyson Lackey <lackeyt90@gmail.com>
2020-07-17 08:17:40 -0700

    create initial project files

---
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Template.Models;
+using System;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadTests //: IDisposable
+  {
+
+    // public void Dispose()
+    // {
+    //   Item.ClearAll();
+    // }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      // Item newItem = new Item("test");
+      // Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+  }
+}
---
+++ PierresBakery/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
