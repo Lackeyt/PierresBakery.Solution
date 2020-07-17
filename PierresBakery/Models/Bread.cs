59b35cebd858fb79c6c842eaf48b8f189b7634c6
Tyson Lackey <lackeyt90@gmail.com>
2020-07-17 09:33:56 -0700

    green light confirm valid bread order test

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -6,17 +6,20 @@ namespace PierresBakery.Models
   public class Bread
   {
     public string BreadOrderString { get; set; }
+    public int BreadOrderInt;
     public int BreadCost { get; set; }
 
     public Bread(string UserBreadOrder)
     {
       BreadOrderString = UserBreadOrder;
+      BreadOrderInt = 0;
       BreadCost = 0;
     }
 
     public bool IsBreadOrderValid()
     {
-      return false;
+      bool isInt = int.TryParse(BreadOrderString, out BreadOrderInt);
+      return isInt;
     }
 
     public void BreadCostCalc()
